6f1f2289cde976682c02dbd5556823d73d033489
Artak1999 <dashyan.1999@mail.ru>
2022-07-04 14:24:58 +0500

    Very interesting tasks.

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long firstName = 1000;
+        int secondName = (int)firstName;
+        System.out.println(firstName);
+        System.out.println(secondName);
     }
 }
